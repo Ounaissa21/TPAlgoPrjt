652f9df860eb0cb9f7c345f8970f428d2410cadc
MELLITI Ounaissa <pikatchousawsan@gmail.com>
2024-01-14 18:10:00 +0100

    la fenetre principale

--- fusion.c
+++ fusion.c
@@ -1,169 +1,29 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#define N 50
+static void on_activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Tri par fusion");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1354, 650);
 
-int taille_tableau;
-int tableau[N];
-GtkWidget *labels[N];
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), box);
 
-void merge(int tab[], int i1, int j1, int i2, int j2);
-void tri_fusion(int tab[], int debut, int fin);
+  //  GtkWidget *button1 = gtk_button_new_with_label("Exécuter l'algorithme");
+  //  gtk_box_pack_start(GTK_BOX(box), button1, TRUE, TRUE, 0);
 
-void creer_interface(int nb) {
-	
-    GtkWidget *window, *vbox, *button;
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	
-    gtk_window_set_title(GTK_WINDOW(window), "Visualisation du tri par fusion");
-	
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 200);
-	
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-	
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-	
-	GtkWidget *button = gtk_button_new();
-	
-	GtkWidget *colorbutton = gtk_color_button_new();
-
-    for (int i = 0; i < nb; i++) {
-		
-        labels[i] = gtk_label_new(" ");
-		
-        gtk_widget_set_size_request(labels[i], 40, 20);
-		
-        gtk_box_pack_start(GTK_BOX(vbox), labels[i], FALSE, FALSE, 5);
-		
-    }
-
-    button = gtk_button_new_with_label("Tri");
-	
-    g_signal_connect(button, "clicked", G_CALLBACK(tri_fusion), (gpointer)nb);
-	
-    gtk_box_pack_start(GTK_BOX(vbox), button, TRUE, TRUE, 5);
-	
-	gtk_color_button_set_title(GTK_COLOR_BUTTON(Blush Pink), title);
+   // GtkWidget *button2 = gtk_button_new_with_label("Quitter");
+    //gtk_box_pack_start(GTK_BOX(box), button2, TRUE, TRUE, 0);
 
     gtk_widget_show_all(window);
 }
 
-void affiche_tableau(int tab[], int debut, int fin) {
-	
-    for (int i = debut; i <= fin; i++) {
-		
-        char ch[20];
-		
-        sprintf(ch, "%d", tab[i]);
-		
-        gtk_label_set_text(GTK_LABEL(labels[i]), ch);
-    }
-	
-    gtk_widget_show_all(GTK_WIDGET(labels[0]));
-}
-
-void tri_fusion(int tab[], int debut, int fin) {
-	
-    int i, j, k, mid;
-
-    if (debut < fin) {
-        mid = (debut + fin) / 2;
-
-        tri_fusion(tab, debut, mid);
-        tri_fusion(tab, mid + 1, fin);
-
-        merge(tab, debut, mid, mid + 1, fin);
-    }
-}
-
-void merge(int tab[], int i1, int j1, int i2, int j2) {
-    int tab1[j1 - i1 + 1], tab2[j2 - i2 + 1];
-    int i, j, k;
-
-    for (i = i1; i <= j1; i++)
-        tab1[i - i1] = tab[i];
-    for (j = i2; j <= j2; j++)
-        tab2[j - i2] = tab[j];
-
-    i = 0;
-    j = 0;
-    k = i1;
-
-    while (i <= j1 - i1 && j <= j2 - i2) {
-        if (tab1[i] <= tab2[j]) {
-            tab[k] = tab1[i];
-            i++;
-        } else {
-            tab[k] = tab2[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i <= j1 - i1) {
-        tab[k] = tab1[i];
-        i++;
-        k++;
-    }
-
-    while (j <= j2 - i2) {
-        tab[k] =0; j++; k++; }
-
-        affiche_tableau(tab, i1, k - 1);
-     }
-	 
-	 GtkWidget *colorbutton = gtk_color_button_new();
-	 
-	 gtk_color_button_set_title(GTK_COLOR_BUTTON(Magenta Purple), title);
-	 
-}
-	 
 int main(int argc, char *argv[]) {
-	
-     GtkWidget *window;
-	 
-     gtk_init(&argc, &argv);
+    GtkApplication *app = gtk_application_new("com.example.triparfusion", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
 
-taille_tableau = N;
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
 
-for (int i = 0; i < N; i++) {
-	
-    tableau[i] = rand() % N;
-}
+    g_object_unref(app);
 
-creer_interface(N);
-
-for (int i = 0; i < N; i++) {
-	
-    char ch[20];
-	
-    gtk_label_set_text(GTK_LABEL(labels[i]), ch);
-	
-	GtkWidget *combobox = gtk_combo_box_new();
-	
-	GtkWidget *combobox = gtk_combo_box_new_with_entry();
-	
-	gtk_combo_box_set_active(GTK_COMBOBOX(labels),i);
-	
-}
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-	GtkWidget *button = gtk_button_new();
-	
-	GtkWidget *colorbutton = gtk_color_button_new();
-	
-    gtk_widget_show_all(GTK_WIDGET(labels[0]));
-
-
-    gtk_main();
-
-    
-	return 0;
+    return status;
 }
