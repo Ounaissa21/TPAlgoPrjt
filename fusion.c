2854aee3dde68d03c1d48fb1c797889a596e4c3a
MELLITI Ounaissa <pikatchousawsan@gmail.com>
2024-01-14 18:18:23 +0100

    couleur du fond de fenetre

--- fusion.c
+++ fusion.c
@@ -5,15 +5,16 @@ static void on_activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_title(GTK_WINDOW(window), "Tri par fusion");
     gtk_window_set_default_size(GTK_WINDOW(window), 1354, 650);
 
+    // Créer une boîte
     GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
     gtk_container_add(GTK_CONTAINER(window), box);
 
-  //  GtkWidget *button1 = gtk_button_new_with_label("Exécuter l'algorithme");
-  //  gtk_box_pack_start(GTK_BOX(box), button1, TRUE, TRUE, 0);
+    // Modifier la couleur de fond de la fenêtre en vert menthe foncé
+    GdkRGBA color;
+    gdk_rgba_parse(&color, "rgb(1, 65, 52)");  // Ces valeurs correspondent à un vert menthe plus foncé
+    gtk_widget_override_background_color(window, GTK_STATE_FLAG_NORMAL, &color);
 
-   // GtkWidget *button2 = gtk_button_new_with_label("Quitter");
-    //gtk_box_pack_start(GTK_BOX(box), button2, TRUE, TRUE, 0);
-
+    // Afficher la fenêtre
     gtk_widget_show_all(window);
 }
 
